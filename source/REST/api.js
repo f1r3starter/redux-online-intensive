73689135e66d3eba9df7d7ba600b4a872b6ff3eb
f1r3starter <andrey.filenochko@gmail.com>
2019-02-16 23:58:54 +0200

    like/unlike

--- source/REST/api.js
+++ source/REST/api.js
@@ -70,5 +70,14 @@ export const api = {
                 },
             });
         },
+        like (postId) {
+            return fetch(`${MAIN_URL}/feed/like/${postId}`, {
+                method:  'PUT',
+                headers: {
+                    Authorization:  this.token,
+                    'Content-Type': 'application/json',
+                },
+            });
+        },
     },
 };
--- source/bus/posts/actions.js
+++ source/bus/posts/actions.js
@@ -26,6 +26,18 @@ export const postsActions = {
             payload: postId,
         };
     },
+    likePost: (likedPostData) => {
+        return {
+            type:    types.LIKE_POST,
+            payload: likedPostData,
+        };
+    },
+    unlikePost: (unlikedPostData) => {
+        return {
+            type:    types.UNLIKE_POST,
+            payload: unlikedPostData,
+        };
+    },
 
     // Async
     fetchPostsAsync: () => {
@@ -42,6 +54,18 @@ export const postsActions = {
     removePostAsync: (postId) => {
         return {
             type:    types.REMOVE_POST_ASYNC,
+            payload: postId,
+        };
+    },
+    likePostAsync: (postId) => {
+        return {
+            type:    types.LIKE_POST_ASYNC,
+            payload: postId,
+        };
+    },
+    unlikePostAsync: (postId) => {
+        return {
+            type:    types.UNLIKE_POST_ASYNC,
             payload: postId,
         };
     },
